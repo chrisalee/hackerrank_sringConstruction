1141ab6ae3f3ae6b3249237fb69b101fb4290a71
Christopher L <56335217+chrisalee@users.noreply.github.com>
2023-03-24 14:32:57 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+ * Complete the 'stringConstruction' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function stringConstruction(s) {
+    let cost = 0;
+    for(let i = 0; i < s.length; i++) {
+        if(!s.slice(0, i).includes(s[i])) {
+            cost ++;
+        }
+    }
+    console.log('cost: ', cost)
+    return cost;
+}
